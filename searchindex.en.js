09d1773c426925fb1ef2fa2089d595ecdbaf8120
aden <surya.kejawen@gmail.com>
2025-05-21 21:20:56 +0700

    try to update index

--- searchindex.en.js
+++ searchindex.en.js
@@ -9,8 +9,8 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "",
-    "content": "",
-    "description": "",
+    "content": "Lawang Api Gateway Platform adalah Api Gateway yang difokuskan pada flektibilitas, performa, dan keamanan. Lawang Api Gateway Platform memungkinkan Anda untuk meng-expose berbagai jenis layanan, seperti database, file, http, grpc, graphql, bahkan dapat membuat custom layanan dengan mudah menggunakan JavaScript.\nFitur Utama High Performance: Didesain menggunakan teknologi yang canggih untuk memberikan performa yang tinggi.\nKeamanan: Dilengkapi dengan fitur keamanan baik authentication dan authorization menggunakan Basic Authentication, Api Key maupun JWT. Selain itu, Lawang Api Gateway Platform juga menerapkan Zero Trust untuk memastikan keamanan transmisi data.\nMultiple Backend: Mengakomodir berbagai jenis backend seperti database, file, http, grpc, graphql, dan custom menggunakan JavaScript.\nLow Latency: Api Gateway Anda tidak menjadi bottleneck pada sistem Anda.\nHigh Availability: Support high availability menggunakan load balancer.\nScalable: Mudah di-scale sesuai dengan kebutuhan Anda menggunakan horizontal scaling mausih vertical scaling.\nMonitoring: Dashboard yang memudahkan Anda untuk memantau kinerja layanan Anda.",
+    "description": "Lawang Api Gateway Platform adalah Api Gateway yang difokuskan pada flektibilitas, performa, dan keamanan. Lawang Api Gateway Platform memungkinkan Anda untuk meng-expose berbagai jenis layanan, seperti database, file, http, grpc, graphql, bahkan dapat membuat custom layanan dengan mudah menggunakan JavaScript.\nFitur Utama High Performance: Didesain menggunakan teknologi yang canggih untuk memberikan performa yang tinggi.\nKeamanan: Dilengkapi dengan fitur keamanan baik authentication dan authorization menggunakan Basic Authentication, Api Key maupun JWT. Selain itu, Lawang Api Gateway Platform juga menerapkan Zero Trust untuk memastikan keamanan transmisi data.",
     "tags": [],
     "title": "Lawang Api Gateway Platform",
     "uri": "/lawang-docs/index.html"
