5c0881a56dabda4cd0ff0e20829888464a917eb2
aden <surya.kejawen@gmail.com>
2025-05-21 20:42:17 +0700

    first build

---
+++ searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/lawang-docs/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Site",
+    "uri": "/lawang-docs/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/lawang-docs/tags/index.html"
+  }
+]
